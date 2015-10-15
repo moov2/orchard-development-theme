fde0c807d4e69b0078a0cd01f95a0a71e08bd674
Peter Keating <peter@moov2.com>
2015-10-15 17:52:38 +0100

    Setup Sass

--- gruntfile.js
+++ gruntfile.js
@@ -24,6 +24,7 @@ module.exports = function(grunt) {
     };
 
     grunt.initConfig({
+        
         /**
          * ------
          * Configuration properties.
@@ -32,12 +33,64 @@ module.exports = function(grunt) {
 
         // configuration values used to drive the build.
         config: config,
+        
+        /**
+         * ------
+         * Build tasks.
+         * ------
+         */
+        
+        /**
+         * Performs tasks (e.g. optimisation) to CSS file compiled by CSS.
+         */
+        postcss: {
+            options: {
+                processors: [require('autoprefixer')({browsers: 'last 1 version'})]
+            },
+            dev: { src: '<%= config.paths.styles %>/*.css' },
+            dist: { src: '<%= config.paths.dist %>/Styles/*.css' }
+        },
+        
+        /**
+         * Handles compiling Sass to CSS.
+         */
+        sass: {
+            options: {
+                sourceMap: false
+            },
+
+            /**
+             * Development version will be uncompressed.
+             */
+            dev: {
+                files: {
+                    '<%= config.paths.styles %>/Site.css': '<%= config.paths.styles %>/Site.scss'
+                }
+            },
+
+            /**
+             * Distributable version will be compressed.
+             */
+            dist: {
+                options: { outputStyle: 'compressed' },
+                files: {
+                    '<%= config.paths.dist %>/Styles/Site.css': '<%= config.paths.styles %>/Site.scss'
+                }
+            }
+        },
     });
 
     // third party plugins used in the build process.
-
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-sass');
 
     // build tasks.
+    
+    /**
+     * Handles creation of CSS files.
+     */
+    grunt.registerTask('styles', ['sass:dev', 'postcss:dev']);
+    grunt.registerTask('styles:dist', ['sass:dist', 'postcss:dist']);
 
     /**
      * Default task should be run while developing on the theme.
