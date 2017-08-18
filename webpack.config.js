b20030a0b42115163ddbe0e804c7794e1a5eda83
mobinzk <mobinzk@gmail.com>
2017-08-18 15:50:40 +0100

    Add jshint to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const webpack = require('webpack');
+
 module.exports = {
 	entry: './Scripts/index.js',
     output: {
@@ -13,7 +15,27 @@ module.exports = {
 				exclude: /(node_modules)/,
 				loaders: ['babel-loader']
 			}
-		]
+		],
+		rules: [{
+            test: /\.js$/, // include .js files
+            enforce: "pre", // preload jshint loader (must be used as a preloader aka before all other loaders.)
+            exclude: /node_modules/, // exclude any and all files in the node_modules folder
+            use: [{
+				loader: "jshint-loader",
+            	options: {
+                    // any jshint option http://www.jshint.com/docs/options/
+                    // i. e.
+                    camelcase: true,
+                    // jshint errors are displayed by default as warnings
+                    // set emitErrors to true to display them as errors
+                    emitErrors: false,
+                    // jshint to not interrupt the compilation
+                    // if you want any file with jshint errors to fail
+                    // set failOnHint to true
+                    failOnHint: false
+                }
+            }]
+        }]
 	},
     resolve: {
     	modules: [
