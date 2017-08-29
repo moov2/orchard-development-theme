b87818f6c1b09f69f9815c8930e0e23456ce063d
Peter Keating <peter@moov2.com>
2017-08-29 10:28:40 +0100

    Use webpack for CSS compilation
    Instead of having separate commands for the different things we wish to
    do to compile a CSS stylesheet, it's all now being handled by webpack.
    Using `npm run bundle`, will compile both CSS and JS into single files.
    For development, using `npm run bundle:watch` will run the compilation
    and then continue to watch the source files for changes to trigger
    the compilation.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
 const path = require('path')
+const ExtractTextPlugin = require('extract-text-webpack-plugin')
+const autoprefixer = require('autoprefixer')
 
 module.exports = [{
+  name: 'js',
   entry: './Scripts/index.js',
   output: {
     path: path.resolve(__dirname, '/Scripts'),
@@ -17,6 +20,7 @@ module.exports = [{
     modules: ['./Scripts/src']
   }
 }, {
+  name: 'js-tests',
   entry: './Scripts/tests/suite.js',
   output: {
     path: path.resolve(__dirname, '/Scripts/tests'),
@@ -35,4 +39,35 @@ module.exports = [{
       './Scripts/tests/src'
     ]
   }
+}, {
+  name: 'css',
+  entry: {
+    main: './Styles/Site.scss'
+  },
+  output: {
+    path: path.resolve(__dirname, './Styles'),
+    filename: 'Site.css'
+  },
+  module: {
+    rules: [{
+      test: /\.scss$/,
+      use: ExtractTextPlugin.extract({
+        use: [{
+          loader: 'css-loader'
+        }, {
+          loader: 'postcss-loader',
+          options: {
+            plugins: function () {
+              return [autoprefixer('last 1 version', 'ie 10')]
+            }
+          }
+        }, {
+          loader: 'sass-loader'
+        }]
+      })
+    }]
+  },
+  plugins: [
+    new ExtractTextPlugin('Site.css')
+  ]
 }]
